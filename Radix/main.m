5426d5bac34830662bb29ccb5da97a2379084b4f
Collin Ruffenach <collin@notionhq.com>
2013-03-18 19:20:07 -0700

    Initial Commit

---
+++ Radix/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Radix
+//
+//  Created by Collin Ruffenach on 3/18/13.
+//  Copyright (c) 2013 Notion HQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NHQAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NHQAppDelegate class]));
+    }
+}
